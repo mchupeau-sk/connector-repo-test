4f36e800b7753b29c1a43dba89338005242ed0cc
mchupeau-sk <mehidine.chupeau@sekoia.io>
2025-09-04 13:46:41 +0000

    update

--- CrowdStrikeFalcon/main.py
+++ CrowdStrikeFalcon/main.py
@@ -12,14 +12,5 @@ from crowdstrike_falcon.host_actions import CrowdstrikeActionDeIsolateHosts, Cro
 if __name__ == "__main__":
     module = CrowdStrikeFalconModule()
     module.register(EventStreamTrigger, "event_stream_trigger")
-    module.register(CrowdstrikeActionPushIOCsBlock, "push_iocs_block")
-    module.register(CrowdstrikeActionPushIOCsDetect, "push_iocs_detect")
-    module.register(CrowdstrikeActionBlockIOC, "block_ioc")
-    module.register(CrowdstrikeActionMonitorIOC, "monitor_ioc")
-    module.register(CrowdstrikeActionIsolateHosts, "isolate_hosts")
-    module.register(CrowdstrikeActionDeIsolateHosts, "deisolate_hosts")
-
-    module.register(CrowdstrikeActionUpdateAlertStatus, "alert_update_status")
-    module.register(CrowdstrikeActionCommentAlert, "alert_add_comment")
 
     module.run()
